502bc3d3db86fdf637237a88005b923a2212fa5e
Umesh004 <umeshreddykonada@gmail.com>
2025-01-26 19:09:58 +0530

    Deployment changes

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,7 +1,7 @@
-export const host = "http://localhost:5000";
+export const host = "https://chat-application-api-theta.vercel.app";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const setAvatarRoute = `${host}/api/auth/setavatar`;
-export const allUsersRoute= `${host}/api/auth/allusers`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
 export const sendMessageRoute = `${host}/api/messages/addmsg`;
-export const getAllMessagesRoute = `${host}/api/messages/getmsg`
+export const getAllMessagesRoute = `${host}/api/messages/getmsg`;
